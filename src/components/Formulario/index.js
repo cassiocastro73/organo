27530a1d52a9a9e4a918abfaac73d55a4e8bb6c3
Cássio de Castro <cassiocastro222@gmail.com>
2023-07-20 12:38:32 -0300

    Prevenindo comportamento default do botão

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -5,13 +5,24 @@ import './Formulario.css'
 
 const Formulario = () => {
     const times = [
-        'Programação', 'Front-End', 'DevOps', 'UX Design'
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
     ];
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido!')
+    }
 
     return (
 
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
