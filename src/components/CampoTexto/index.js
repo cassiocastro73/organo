24a02bf98d66ebae894b887adc43008741edca23
Cássio de Castro <cassiocastro222@gmail.com>
2023-07-20 12:55:51 -0300

    Validação de campos obrigatórios

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -4,12 +4,16 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...` 
 
+    
+
+    
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={placeholderModificada}/>
+            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
--- src/components/ListaSuspensa/index.js
+++ src/components/ListaSuspensa/index.js
@@ -2,10 +2,12 @@ import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
     return (
-    <div className='lista-suspensa'>
-        <label>{props.label}</label> 
-        <select>
-            {props.itens.map(item => <option key={item}>{item}</option>)}
+        <div className='lista-suspensa'>
+        <label>{props.label}</label>
+        <select required={props.required}>
+            {props.itens.map(item => {
+                return <option key={item}>{item}</option>
+            })}
         </select>
     </div>
   );
