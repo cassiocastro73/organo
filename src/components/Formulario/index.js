89ad878814b856362ddd5d016742451a84c2a6e7
Cássio de Castro <cassiocastro222@gmail.com>
2023-07-21 10:02:15 -0300

    Criação do objeto colaborador

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -5,7 +5,7 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         'Programação',
         'Front-End',
@@ -25,7 +25,13 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido!', nome, cargo, imagem, time)
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+
     }
 
     return (
