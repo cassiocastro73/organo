9ecbbbea2bd9f182270b042d9c103fe326f8b09d
Cássio de Castro <cassiocastro222@gmail.com>
2023-07-25 11:49:36 -0300

    Adição das sections de times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/Time';
+
 
 function App() {
 
@@ -18,6 +20,13 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
+      <Time nome="Data Science"/>
+      <Time nome="DevOps"/>
+      <Time nome="UX e Design"/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação e Gestão"/>
     </div>
   );
 }
